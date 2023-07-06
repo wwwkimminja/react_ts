10b5e560490f798ecff79feb146171e53fb5cf14
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2023-07-06 19:19:31 +0900

    creat project

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from "styled-components";
+import App from './App';
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+
+const lightTheme = {
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
+};
+
+
+//@ts-ignore
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={lightTheme}>
+    <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
+
+
