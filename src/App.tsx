2a7cc28906ca7843cdffbbca6657c6d948624af3
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2023-07-07 19:50:11 +0900

    BrouwerRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import styled from "styled-components"
+import Router from "./screens/Router";
 
-const Container=styled.div`
-background-color: ${(props)=>props.theme.bgColor};
-`;
-const H1=styled.h1`
-color:${(props)=>props.theme.textColor};
-`;
 
 function App() {
  
   return (
-      <Container>
-        <H1>Hello</H1>
-      </Container>
+   <>
+   <Router/>
+   </>
   )
 }
 
