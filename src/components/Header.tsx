6c9826373a8a6d1f38935449223d9c7b773b4725
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2023-07-07 20:54:25 +0900

    using useNavigate()

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Header() {
+  const navigate=useNavigate();
+  const onAboutClick=()=>{
+    navigate("/about")
+  }
   return (
     <header>
       <ul>
         <li><Link to={"/"}>Home</Link></li>
-        <li><Link to={"/about"}>About</Link></li>
+        <li><button onClick={onAboutClick}>About</button></li>
       </ul>
     </header>
   )
