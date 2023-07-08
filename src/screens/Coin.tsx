dffa59d175bb3de4155ce384cbfa753b204adb94
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2023-07-08 20:11:30 +0900

    declare type :fetch data

--- src/screens/Coin.tsx
+++ src/screens/Coin.tsx
@@ -2,11 +2,76 @@ import React, { useState,useEffect } from 'react'
 import { Container, Header ,Title , Loader} from './Coins'
 import { useLocation, useParams } from 'react-router-dom';
 
+interface IInfoData{
+  id:string;
+  name:string;
+  symbol:string;
+  rank:number;
+  is_new:boolean;
+  is_active:boolean;
+  type:string;
+  logo:string;
+  tags:object;
+  team:object;
+  description:string;
+  message:string;
+  open_source:boolean;
+  started_at:string;
+  development_status:string;
+  hardware_wallet:boolean;
+  proof_type:string;
+  org_structure:string;
+  hash_algorithm:string;
+  links:object;
+  links_extended:object;
+  whitepaper:object;
+  first_data_at:string;
+  last_data_at:string;
+
+}
+ interface IPriceData {
+  id:                 string;
+  name:               string;
+  symbol:             string;
+  rank:               number;
+  circulating_supply: number;
+  total_supply:       number;
+  max_supply:         number;
+  beta_value:         number;
+  first_data_at:      Date;
+  last_updated:       Date;
+  quotes:             Quotes;
+}
+
+ interface Quotes {
+  USD: Usd;
+}
+
+ interface Usd {
+  price:                  number;
+  volume_24h:             number;
+  volume_24h_change_24h:  number;
+  market_cap:             number;
+  market_cap_change_24h:  number;
+  percent_change_15m:     number;
+  percent_change_30m:     number;
+  percent_change_1h:      number;
+  percent_change_6h:      number;
+  percent_change_12h:     number;
+  percent_change_24h:     number;
+  percent_change_7d:      number;
+  percent_change_30d:     number;
+  percent_change_1y:      number;
+  ath_price:              number;
+  ath_date:               Date;
+  percent_from_price_ath: number;
+}
+
 
 export default function Coin() {
   const [loading,setLoading]=useState(true);
-  const [info,setInfo]=useState({});
-  const [priceInfo,setPriceInfo]=useState({});
+  const [info,setInfo]=useState<IInfoData>();
+  const [priceInfo,setPriceInfo]=useState<IPriceData>();
    const {coinId}= useParams();
   const {state} =useLocation();
 
